d3d17a0fa3841177e5a45c3f264f37a2118d6882
yangdangfu <yangdangfu@csu.edu.cn>
2021-08-16 15:44:36 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayGoodbye = exports.sayHello = void 0;
+var hello_1 = require("./hello");
+Object.defineProperty(exports, "sayHello", { enumerable: true, get: function () { return hello_1.sayHello; } });
+Object.defineProperty(exports, "sayGoodbye", { enumerable: true, get: function () { return hello_1.sayGoodbye; } });
